ed497e80acbf0941306688df896ab9c9c90c45da
mdsaad80 <mdsaad80@gmail.com>
2019-04-28 11:39:34 +0530

    Create final_28_04.py

---
+++ final_28_04.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+from __future__ import division
+#!/usr/bin/env python
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import glob
+import argparse
+import requests
+import picamera
+from PIL import Image
+import json
+import time
+import math
+import os
+import json
+import sys
+from time import sleep
+from fractions import Fraction as F
+
+ 
+def main():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)
+        camera.framerate = F(1 , 2)
+        camera.start_preview()
+        for filename in camera.capture_continuous('img{counter:03d}.jpg'):
+            result=[]
+            #time.sleep(1)
+            print('Captured video stream as frame with name %s' % filename)
+            path = "/home/pi/Desktop/Apna/%s" % filename
+            with open(path, 'rb') as fp:
+                response = requests.post(
+                'https://api.platerecognizer.com/v1/plate-reader/',
+                files=dict(upload=fp),
+                headers={'Authorization': 'Token ' + '90ab0162c21a21e1147936f45c7566ecd0ce7c8a'})
+                result.append(response.json())
+                print(response.json())
+                    #print(path)
+                    #time.sleep(1)
+                with open('data.json', 'w') as outfile:
+                    json.dump(result, outfile)
+                    header = {}
+                    #print(path)
+                    new_plate = result[0]['results'][0]['plate']
+                    print(new_plate)
+                    payload = {"plate_number": result[0]['results'][0]['plate'], "camera_id":'1234'}
+                    fin = open(path, 'rb')
+                    files = {'plate_image': fin}
+                    try:
+                        req = requests.post("http://192.168.43.224:5000/api/numberplate/add", headers=header, data= payload, files = files )
+                    except requests.exceptions.RequestException as e:
+                        print(e)
+                        sys.exit(1)
+                    print(req.status_code, req.reason)
+            os.remove("%s" %filename)
+            #time.sleep(1)
+            #print(json.dumps(result, indent=2))
+    camera.stop_preview()     
+
+if __name__ == '__main__':
+    main()
+      
